8180e1b4c23dbcf907b12503660c77a0a981dc3f
Radhika <sach.radhika@googlemail.com>
2019-02-28 14:31:07 +0100

    React redux basic

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+import { combineReducers } from 'redux';
+import BookReducer from './reducer_book';
+import ActiveBook from './reducer_active_book'
+
+const rootReducer = combineReducers({
+  books: BookReducer,
+  activeBook: ActiveBook
+});
+
+/*render(
+  <Provider store={rootReducer}>
+    <App />
+  </Provider>
+);*/
+
+export default rootReducer;
